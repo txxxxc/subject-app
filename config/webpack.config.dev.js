0700739a7a54e02bdd8ae07dacd5c7df643a975e
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-09-26 22:26:03 +0900

    hoge

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,17 +1,17 @@
-const merge = require('webpack-merge');
-const webpackConfig = require('./webpack.config.js');
-module.exports = merge(webpackConfig, {
-  mode: 'development',
-  devServer: {
-    historyApiFallback: true,
-    inline: true,
-    host: 'subject-app.herokuapp.com',
-    proxy: {
-      '/api/**': {
-        target: 'https://subject-app.herokuapp.com',
-        secure: false,
-        logLevel: 'debug'
-      }
-    }
-  }
-});
+// const merge = require('webpack-merge');
+// const webpackConfig = require('./webpack.config.js');
+// module.exports = merge(webpackConfig, {
+//   mode: 'development',
+//   devServer: {
+//     historyApiFallback: true,
+//     inline: true,
+//     host: 'subject-app.herokuapp.com',
+//     proxy: {
+//       '/api/**': {
+//         target: 'https://subject-app.herokuapp.com',
+//         secure: false,
+//         logLevel: 'debug'
+//       }
+//     }
+//   }
+// });
