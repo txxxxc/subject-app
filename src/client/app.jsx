97d5b584c53ec1e9c8005677a39c90a5de6fffde
Tanaka-Tomoya <tomoya2627@gmail.com>
2019-09-26 20:26:36 +0900

    error

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -16,9 +16,10 @@ import styled, {
 import Home from './container/templates/Home';
 import './reset.css';
 import theme from './config/theme';
-const port = (process.env.NODE_ENV = production
+
+const port = process.env.NODE_ENV
   ? `${process.env.PORT}/graphql`
-  : 'http://localhost:4000/graphql');
+  : 'http://localhost:4000/graphql';
 
 const httpLink = createHttpLink({
   uri: port,
